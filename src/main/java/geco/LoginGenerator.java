159f2564125091fb9a514b1d884adb350bdee0e0
dorian-pixel <dorian.azema@gmail.com>
2019-11-27 08:15:21 +0100

    login-generator-8 Correction bug Generation Login close #8

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -36,7 +36,13 @@ public class LoginGenerator {
      */
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
         String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
+        String n = "";
+        if (nom.length() < 3) {
+            n = deAccent(nom.substring(0,2).toUpperCase());
+        } else {
+            n = deAccent(nom.substring(0,3).toUpperCase());
+        }
+
         String login = p+n ;
 
         if (loginService.loginExists(login)) {
